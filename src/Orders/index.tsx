d30cf44275660eeeca4f252fb951c6c9c5bea298
iamgrodrigues <iamgrodrigues@gmail.com>
2021-01-27 15:54:33 -0300

    wip: Add make order

--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -1,23 +1,29 @@
 import { useEffect, useState } from 'react'
-import './styles.css'
+import { toast } from 'react-toastify';
 import StepsHeader from './StepsHeader'
 import ProductList from './ProductList'
 import { OrderLocationData, Product } from './types'
-import { fetchProducts } from '../api'
+import { fetchProducts, saveOrder } from '../api'
 import OrderLocation from './OrderLocation'
 import OrderSummary from './OrderSummary'
 import Footer from '../Footer'
 import { checkIfIsSelected } from './helpers'
+import './styles.css'
 
 function Orders() {
   const [products, setProducts] = useState<Product[]>([]);
   const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
   const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+  const totalPrice = selectedProducts.reduce((sum, item) => {
+    return sum + item.price;
+  }, 0);
 
   useEffect(() => {
     fetchProducts()
       .then(response => setProducts(response.data))
-      .catch(error => console.log(error))
+      .catch(error => {
+        toast.warning('Erro ao listar produtos');
+      })
   }, []);
 
   const handleSelectProduct = (product: Product) => {
@@ -31,6 +37,23 @@ function Orders() {
     }
   }
 
+  const handleSubmit = () => {
+    const productsIds = selectedProducts.map(({ id }) => ({ id }));
+    const payload = {
+      ...orderLocation!,
+      products: productsIds
+    }
+  
+    saveOrder(payload)
+      .then((response) => {
+        toast.error(`Pedido enviado com sucesso! Nº ${response.data.id}`);
+        setSelectedProducts([]);
+      })
+      .catch(() => {
+        toast.warning('Erro ao enviar pedido');
+      })
+  }
+
   return (
     <>
       <div className="orders-container">
@@ -42,7 +65,11 @@ function Orders() {
         />
         <OrderLocation
           onChangeLocation={location => setOrderLocation(location)} />
-        <OrderSummary />
+        <OrderSummary 
+          amount={selectedProducts.length}
+          totalPrice={totalPrice}
+          onSubmit={handleSubmit}
+        />
       </div>
       <Footer />
     </>
